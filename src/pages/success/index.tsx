91fb808d59f8e3ddc19882ed3e9a3e059a623ead
anjaemin <t01089416422@gmail.com>
2022-10-14 19:45:57 +0900

    test: SuccessPageButtonSection

--- src/pages/success/index.tsx
+++ src/pages/success/index.tsx
@@ -2,4 +2,8 @@ import React from 'react';
 
 import * as S from './styled';
 
-export const SuccessPage: React.FC = () => <S.SuccessPageContainer></S.SuccessPageContainer>;
+export const SuccessPage: React.FC = () => (
+  <S.SuccessPageContainer>
+    <S.SuccessPageButtonSection></S.SuccessPageButtonSection>
+  </S.SuccessPageContainer>
+);
--- src/pages/success/styled.ts
+++ src/pages/success/styled.ts
@@ -1,3 +1,5 @@
 import styled from '@emotion/styled';
 
 export const SuccessPageContainer = styled.div``;
+
+export const SuccessPageButtonSection = styled.div``;
