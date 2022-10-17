139b1c46e2b3ca3d1d6119a9b4e61a466616d788
anjaemin <t01089416422@gmail.com>
2022-10-17 15:53:05 +0900

    fix: button

--- src/pages/download/index.tsx
+++ src/pages/download/index.tsx
@@ -6,8 +6,8 @@ import * as S from './styled';
 export const DownloadPage: React.FC = () => (
   <S.DownloadPageContainer>
     <S.DownloadPageButtonSection>
-      <Button bgColor="var(--color-button-primary)" label="다운로드" />
-      <Button bgColor="var(--color-button-secondary)" label="신고" />
+      <Button click={() => {}} bgColor="var(--color-button-secondary)" label="다운로드" />
+      <Button click={() => {}} bgColor="var(--color-button-secondary)" label="신고" />
     </S.DownloadPageButtonSection>
   </S.DownloadPageContainer>
 );
