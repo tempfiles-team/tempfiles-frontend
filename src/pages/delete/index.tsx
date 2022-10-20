d0aa454e75d3d205f33592931e808777680db03c
anjaemin <t01089416422@gmail.com>
2022-10-21 02:26:07 +0900

    feat: Delete Page code

--- src/pages/delete/index.tsx
+++ src/pages/delete/index.tsx
@@ -1,5 +1,50 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
 
+import { Button } from '../../components';
+import { RootState } from '../../state/reducers';
 import * as S from './styled';
 
-export const DeletePage: React.FC = () => <S.DeletePageContainer></S.DeletePageContainer>;
+export const DeletePage: React.FC = () => {
+  const navigate = useNavigate();
+  const DeleteFileName = useSelector((state: RootState) => state.DeleteFileName);
+  useEffect(() => {
+    if (DeleteFileName === '') {
+      navigate(-1);
+    }
+  });
+
+  const deleteFile = async () => {
+    await axios({
+      method: 'delete',
+      url: `https://tfb.minpeter.cf/delete/${DeleteFileName}`,
+    })
+      .then((res) => {
+        console.log(res);
+        toast.success('삭제 완료', {
+          autoClose: 3000,
+          position: toast.POSITION.BOTTOM_RIGHT,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+        toast.error(`삭제 실패 ${err.response.status}`, {
+          autoClose: 3000,
+          position: toast.POSITION.BOTTOM_RIGHT,
+        });
+      });
+  };
+
+  return (
+    <S.DeletePageContainer>
+      <S.DeletePagePageLabel>업로드된 파일을 서버에서 삭제하시겠습니까?</S.DeletePagePageLabel>
+      <S.DeletePageButtonSection>
+        <Button bgColor="var(--color-button-primary)" label="확인" click={deleteFile} />
+        <Button bgColor="var(--color-button-primary)" label="취소" click={() => navigate(-1)} />
+      </S.DeletePageButtonSection>
+    </S.DeletePageContainer>
+  );
+};
