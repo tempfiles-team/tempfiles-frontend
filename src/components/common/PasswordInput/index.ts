076c6c83a169b844f405121eee938d32997292fe
anjaemin <t01089416422@gmail.com>
2022-10-24 17:54:05 +0900

    fix: height 변경

--- src/components/common/PasswordInput/index.ts
+++ src/components/common/PasswordInput/index.ts
@@ -5,7 +5,7 @@ export const PasswordInput = styled.input`
   border-radius: 10px;
   outline: none;
   width: 100%;
-  height: 4.2rem;
+  height: 5rem;
   padding-left: 12px;
   font-size: 1.8rem;
   font-weight: 700;
