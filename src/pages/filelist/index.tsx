746aad17e46a1c3cf6309b72ee3b945f10cd6194
anjaemin <t01089416422@gmail.com>
2022-10-26 06:09:11 +0900

    feat: FileList Page 구성

--- src/pages/filelist/index.tsx
+++ src/pages/filelist/index.tsx
@@ -1,5 +1,40 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
+import { FileListBox } from '../../components';
+import { getFileSize, getShortFileName } from '../../utils';
 import * as S from './styled';
 
-export const FileListPage: React.FC = () => <S.FileListPageContainer></S.FileListPageContainer>;
+export const FileListPage: React.FC = () => {
+  const [fileList, setFileList] = useState<any[]>();
+  const getFileList = async () => {
+    await axios({
+      method: 'get',
+      url: 'https://tfb.minpeter.cf/list',
+    })
+      .then((res) => {
+        setFileList(res.data.list);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  useEffect(() => {
+    getFileList();
+  }, []);
+  return (
+    <>
+      <S.FileListPageContainer>
+        {fileList?.map((item, index) => (
+          <FileListBox
+            key={index}
+            filename={getShortFileName(item.Name)}
+            size={getFileSize(item.Size)}
+            LastModified={new Date(item.LastModified)}
+          />
+        ))}
+      </S.FileListPageContainer>
+      <S.FileListPageBoxShoadow />
+    </>
+  );
+};
--- src/pages/filelist/styled.ts
+++ src/pages/filelist/styled.ts
@@ -1,3 +1,20 @@
 import styled from '@emotion/styled';
 
-export const FileListPageContainer = styled.div``;
+export const FileListPageContainer = styled.div`
+  max-height: 40rem;
+  overflow: auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  &::-webkit-scrollbar {
+    display: none;
+  }
+`;
+
+export const FileListPageBoxShoadow = styled.div`
+  width: 100%;
+  height: 6rem;
+  position: relative;
+  top: -25px;
+  background: linear-gradient(180deg, rgba(40, 42, 58, 0) 0%, #282a3a 45.31%);
+`;
