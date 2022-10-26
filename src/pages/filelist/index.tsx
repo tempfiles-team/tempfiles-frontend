3214eba34109dff5a4ee5729d62dba8a98360056
anjaemin <t01089416422@gmail.com>
2022-10-26 09:43:28 +0900

    fix: 주석, setLoading 값

--- src/pages/filelist/index.tsx
+++ src/pages/filelist/index.tsx
@@ -6,7 +6,7 @@ import { getFileSize, getShortFileName } from '../../utils';
 import * as S from './styled';
 
 export const FileListPage: React.FC = () => {
-  const [loading, setLoading] = useState(true); //loading 확인하고싶으면 false로 바꿔주세요.
+  const [loading, setLoading] = useState(false);
   const SkeletonUIRandomWidth = ['50', '55', '60', '65', '70', '75', '80'];
   const [fileList, setFileList] = useState<any[]>();
   const getFileList = async () => {
@@ -16,7 +16,7 @@ export const FileListPage: React.FC = () => {
     })
       .then((res) => {
         setFileList(res.data.list);
-        setLoading(false);
+        setLoading(true); //loading 확인하고싶으면 false로 바꿔주세요.
       })
       .catch((err) => {
         console.log(err);
