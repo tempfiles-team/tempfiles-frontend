f4f177da526c7117957fb154590ee671f1877177
anjaemin <t01089416422@gmail.com>
2022-10-26 09:41:18 +0900

    feat: SkeletionUI Loading 추가

--- src/pages/filelist/index.tsx
+++ src/pages/filelist/index.tsx
@@ -6,6 +6,8 @@ import { getFileSize, getShortFileName } from '../../utils';
 import * as S from './styled';
 
 export const FileListPage: React.FC = () => {
+  const [loading, setLoading] = useState(true); //loading 확인하고싶으면 false로 바꿔주세요.
+  const SkeletonUIRandomWidth = ['50', '55', '60', '65', '70', '75', '80'];
   const [fileList, setFileList] = useState<any[]>();
   const getFileList = async () => {
     await axios({
@@ -14,6 +16,7 @@ export const FileListPage: React.FC = () => {
     })
       .then((res) => {
         setFileList(res.data.list);
+        setLoading(false);
       })
       .catch((err) => {
         console.log(err);
@@ -24,17 +27,48 @@ export const FileListPage: React.FC = () => {
   }, []);
   return (
     <>
-      <S.FileListPageContainer>
-        {fileList?.map((item, index) => (
-          <FileListBox
-            key={index}
-            filename={getShortFileName(item.Name)}
-            size={getFileSize(item.Size)}
-            LastModified={new Date(item.LastModified)}
-          />
-        ))}
-      </S.FileListPageContainer>
-      <S.FileListPageBoxShoadow />
+      {loading ? (
+        <>
+          <S.FileListPageContainer>
+            {fileList?.map((item, index) => (
+              <FileListBox
+                key={index}
+                filename={getShortFileName(item.Name)}
+                size={getFileSize(item.Size)}
+                LastModified={new Date(item.LastModified)}
+              />
+            ))}
+          </S.FileListPageContainer>
+          <S.FileListPageBoxShoadow />
+        </>
+      ) : (
+        <>
+          <S.FileListPageContainer>
+            <S.FileListSkeletonUI
+              randomWitdh={SkeletonUIRandomWidth[Math.floor(Math.random() * 6)]}
+            />
+            <S.FileListSkeletonUI
+              randomWitdh={SkeletonUIRandomWidth[Math.floor(Math.random() * 6)]}
+            />
+            <S.FileListSkeletonUI
+              randomWitdh={SkeletonUIRandomWidth[Math.floor(Math.random() * 6)]}
+            />
+            <S.FileListSkeletonUI
+              randomWitdh={SkeletonUIRandomWidth[Math.floor(Math.random() * 6)]}
+            />
+            <S.FileListSkeletonUI
+              randomWitdh={SkeletonUIRandomWidth[Math.floor(Math.random() * 6)]}
+            />
+            <S.FileListSkeletonUI
+              randomWitdh={SkeletonUIRandomWidth[Math.floor(Math.random() * 6)]}
+            />
+            <S.FileListSkeletonUI
+              randomWitdh={SkeletonUIRandomWidth[Math.floor(Math.random() * 6)]}
+            />
+          </S.FileListPageContainer>
+          <S.FileListPageBoxShoadow />
+        </>
+      )}
     </>
   );
 };
