a81c6a95b769e7c825e5bc532a3917d724d0b99e
anjaemin <t01089416422@gmail.com>
2022-10-27 18:16:26 +0900

    feat: main, dev branch 나누기 / 제작중 toast 추가

--- src/components/common/Navbar/index.tsx
+++ src/components/common/Navbar/index.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
+import { toast } from 'react-toastify';
 
 import * as S from './styled';
 
 export const Navbar: React.FC = () => (
   <S.NavbarContainer>
     <S.Nav to={'/'}>Upload</S.Nav>
-    <S.Nav to={'/api'}>API</S.Nav>
+    <S.Nav
+      to={'/'}
+      onClick={() => {
+        toast.success('제작중!', {
+          autoClose: 1000,
+          position: toast.POSITION.BOTTOM_RIGHT,
+        });
+      }}
+    >
+      API
+    </S.Nav>
     <S.Nav to={'/filelist'}>File list</S.Nav>
   </S.NavbarContainer>
 );
