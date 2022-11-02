975d04d398560275ba1bc0510fee9338b14fdcee
anjaemin <t01089416422@gmail.com>
2022-11-02 09:39:27 +0900

    feat: SkeletionUI component 추가

--- src/components/common/SkeletonUIBox/index.ts
+++ src/components/common/SkeletonUIBox/index.ts
@@ -1,5 +1,4 @@
 import styled from '@emotion/styled';
-
 export const SkeletonUIBox = styled.div<{ randomWitdh: string }>`
   display: flex;
   background-color: var(--color-backgorund-filelistbox);
@@ -27,8 +26,39 @@ export const SkeletonUIBox = styled.div<{ randomWitdh: string }>`
     }
   }
 `;
-
 export const SkeletonUIApiBox = styled(SkeletonUIBox)`
   min-height: 6.2rem;
   margin: 1rem;
+`;
+
+export const SkeletonUI = styled.div<{
+  width: string;
+  height: string;
+  margin: string;
+}>`
+  display: flex;
+  min-width: ${(props) => props.width};
+  min-height: ${(props) => props.height};
+  margin: ${(props) => props.margin};
+  background: var(--color-backgorund-filelistbox);
+  border-radius: 10px;
+  overflow: hidden;
+  &::before {
+    content: ' ';
+    width: 100%;
+    height: auto;
+    animation: loading 2s infinite;
+    box-shadow: 0 0 30px 30px rgba(255, 255, 255, 0.3);
+  }
+  @keyframes loading {
+    0% {
+      transform: translateX(-50%);
+    }
+    50% {
+      transform: translateX(100%);
+    }
+    100% {
+      transform: translate(200%);
+    }
+  }
 `;
