68ff0731e62e8b05fd446cf6d86ba92bde9388e2
anjaemin <t01089416422@gmail.com>
2022-11-09 08:58:30 +0900

    feat: FileBox component 추가

---
+++ src/components/common/FileBox/index.ts
@@ -0,0 +1,12 @@
+import styled from '@emotion/styled';
+
+export const FileBox = styled.div`
+  background-color: var(--color-backgorund-filelistbox);
+  color: var(--color-text-tertiary);
+  border-radius: 10px;
+  padding: 1.2rem 1.2rem 1.2rem 1.2rem;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  font-size: 2.2rem;
+`;
