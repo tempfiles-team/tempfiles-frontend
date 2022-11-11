98f344244b46d642c8dd76acc2d1e38d96859085
anjaemin <t01089416422@gmail.com>
2022-11-11 09:19:35 +0900

    test: test code

--- src/styles/globalStyle.ts
+++ src/styles/globalStyle.ts
@@ -34,6 +34,8 @@ export const globalStyle = css`
     --mobile-breakpoint: 767px;
     --tablet-breakpoint: 991px;
     --desktop-breakpoint: 1200px;
+
+    --vh: 100%;
   }
   html {
     text-size-adjust: 100%;
@@ -44,7 +46,7 @@ export const globalStyle = css`
     color: var(--color-text-primary);
     letter-spacing: -0.05rem;
     font-size: 1.6rem;
-    height: 100vh;
+    height: calc(var(--vh, 1vh) * 100);
     display: flex;
     flex-direction: column;
     justify-content: center;
