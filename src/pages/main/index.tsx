822019d80185f6f2ecd74f2561ea82b8a55aa32a
anjaemin <t01089416422@gmail.com>
2022-11-17 15:00:35 +0900

    fix: test code 삭제

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -5,7 +5,6 @@ import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import { bindActionCreators } from 'redux';
 
-// eslint-disable-next-line
 import { CheckBox, PasswordInput, UpLoadButton, FileFind, Progress } from '../../components';
 import { actionCreators } from '../../state';
 import { getFileSize } from '../../utils';
