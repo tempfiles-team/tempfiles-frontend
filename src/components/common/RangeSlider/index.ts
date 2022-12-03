3847aff041eb41a5e727be8b09f1ee374c59c107
anjaemin <t01089416422@gmail.com>
2022-12-03 18:53:20 +0900

    feat: RangeSlider components add

--- src/components/common/RangeSlider/index.ts
+++ src/components/common/RangeSlider/index.ts
@@ -6,24 +6,15 @@ export const RangeSlider = styled.input`
   height: 4rem;
   border-radius: 8px;
   background: var(--color-backgorund-black);
+  outline: none;
   &::-webkit-slider-thumb {
     -webkit-appearance: none;
     appearance: none;
     width: 3.9rem;
     height: 4rem;
     background: var(--color-background-secondary);
-    border-radius: 12px;
+    border-radius: 8px;
   }
-  /* &::-webkit-fill-upper {
-    -webkit-appearance: none;
-    appearance: none;
-    background: var(--color-slider-upper);
-  }
-  &::-webkit-fill-lower {
-    -webkit-appearance: none;
-    appearance: none;
-    background: var(--color-slider-lower);
-  } */
 `;
 
 RangeSlider.defaultProps = { type: 'range' };
