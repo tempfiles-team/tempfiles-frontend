7f40db95d77b7263c05f2d0f4b93b8fd11398e8b
anjaemin <t01089416422@gmail.com>
2022-12-03 18:57:51 +0900

    fix: delete_url not found

--- src/pages/delete/index.tsx
+++ src/pages/delete/index.tsx
@@ -20,7 +20,7 @@ export const DeletePage: React.FC = () => {
   const deleteFile = async () => {
     await axios({
       method: 'delete',
-      url: `${DeleteFileProps.delete_url}${
+      url: `${process.env.REACT_APP_BACKEND_BASEURL}${DeleteFileProps.delete_url}${
         DeleteFileProps.isEncrypted ? `?token=${DeleteFileProps.token}` : ''
       }`,
     })
