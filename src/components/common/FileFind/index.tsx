b8185d73529a0cfb21aea2c0efeff44dd974ad11
anjaemin <t01089416422@gmail.com>
2023-02-01 12:04:38 +0900

    feat: file drag&drop upload

--- src/components/common/FileFind/index.tsx
+++ src/components/common/FileFind/index.tsx
@@ -4,12 +4,24 @@ import * as S from './styled';
 
 type FileFindProps = {
   handleChangeFile: any;
+  handleDrop: any;
+  handleDragOver: any;
   fileProps: any;
 };
 
-export const FileFind: React.FC<FileFindProps> = ({ handleChangeFile, fileProps }) => (
+export const FileFind: React.FC<FileFindProps> = ({
+  handleChangeFile,
+  fileProps,
+  handleDrop,
+  handleDragOver,
+}) => (
   <S.FileFindContainer>
-    <S.FileFindTextBox>
+    <S.FileFindLabelBox
+      id="label-file-upload"
+      htmlFor="input-file-upload"
+      onDrop={handleDrop}
+      onDragOver={handleDragOver}
+    >
       {fileProps.filename != '' && fileProps.size != '' && fileProps.fileType != ''
         ? '이름:' +
           fileProps.filename +
@@ -18,9 +30,14 @@ export const FileFind: React.FC<FileFindProps> = ({ handleChangeFile, fileProps 
           ' / 타입:' +
           fileProps.fileType
         : '업로드 할 파일을 선택해주세요....'}
-    </S.FileFindTextBox>
+    </S.FileFindLabelBox>
 
-    <S.FileFindButton htmlFor="file">찾아보기</S.FileFindButton>
-    <input type={'file'} id="file" style={{ display: 'none' }} onChange={handleChangeFile} />
+    <S.FileFindButton htmlFor="input-file-upload">찾아보기</S.FileFindButton>
+    <input
+      id="input-file-upload"
+      type={'file'}
+      style={{ display: 'none' }}
+      onChange={handleChangeFile}
+    />
   </S.FileFindContainer>
 );
