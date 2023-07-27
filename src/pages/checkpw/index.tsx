9db44b067ca66c327fefd2e959a8ebf1c192d155
deopa0402 <deopa0402@gmail.com>
2023-07-27 16:06:48 +0900

    Add password hiding

--- src/pages/checkpw/index.tsx
+++ src/pages/checkpw/index.tsx
@@ -20,7 +20,7 @@ export const CheckPasswordPage: React.FC = () => {
     size: '',
     uploadDate: { year: 0, month: 0, day: 0 },
   });
-  // const { year, month, day } = getDate(checkPasswordFileProps.uploadDate);
+
   const dispatch = useDispatch();
   const { SetDownloadFileProps } = bindActionCreators(actionCreators, dispatch);
 
@@ -33,67 +33,65 @@ export const CheckPasswordPage: React.FC = () => {
         position: toast.POSITION.BOTTOM_RIGHT,
       });
     } else {
-      await axios({
-        method: 'get',
-        url: `${process.env.REACT_APP_BACKEND_BASEURL}/checkpw/${checkfileid}?pw=${password}`,
-      })
-        .then((res) => {
-          SetDownloadFileProps({
-            isEncrypted: true,
-            token: res.data.token,
-          });
-          navigate(`/download/${checkfileid}`);
-          toast.success('통과!', {
-            autoClose: 1000,
-            position: toast.POSITION.BOTTOM_RIGHT,
-          });
-        })
-        .catch((err) => {
-          console.log(err);
-          setPassword('');
-          toast.error('비밀번호를 다시 확인해주세요...', {
-            autoClose: 1000,
-            position: toast.POSITION.BOTTOM_RIGHT,
-          });
+      try {
+        const res = await axios.get(
+          `${process.env.REACT_APP_BACKEND_BASEURL}/checkpw/${checkfileid}?pw=${password}`
+        );
+        SetDownloadFileProps({
+          isEncrypted: true,
+          token: res.data.token,
         });
+        navigate(`/download/${checkfileid}`);
+        toast.success('통과!', {
+          autoClose: 1000,
+          position: toast.POSITION.BOTTOM_RIGHT,
+        });
+      } catch (err) {
+        console.log(err);
+        setPassword('');
+        toast.error('비밀번호를 다시 확인해주세요...', {
+          autoClose: 1000,
+          position: toast.POSITION.BOTTOM_RIGHT,
+        });
+      }
     }
   };
 
   useEffect(() => {
     const getFileProps = async () => {
-      await axios({
-        method: 'get',
-        url: `${process.env.REACT_APP_BACKEND_BASEURL}/file/${checkfileid}`,
-      })
-        .then((res) => {
-          setLoading(false);
-          setFileProps({
-            filename: res.data.filename,
-            size: getFileSize(res.data.size),
-            uploadDate: getDate(res.data.uploadDate),
-          });
-        })
-        .catch((err) => {
-          console.log(err);
-          navigate('/');
-          toast.error('error 문의해주세요...', {
-            autoClose: 1000,
-            position: toast.POSITION.BOTTOM_RIGHT,
-          });
+      try {
+        const res = await axios.get(
+          `${process.env.REACT_APP_BACKEND_BASEURL}/file/${checkfileid}`
+        );
+        setLoading(false);
+        setFileProps({
+          filename: res.data.filename,
+          size: getFileSize(res.data.size),
+          uploadDate: getDate(res.data.uploadDate),
         });
+      } catch (err) {
+        console.log(err);
+        navigate('/');
+        toast.error('error 문의해주세요...', {
+          autoClose: 1000,
+          position: toast.POSITION.BOTTOM_RIGHT,
+        });
+      }
     };
     getFileProps();
   }, [checkfileid, navigate]);
+
   return (
     <S.CheckPasswordPageContainer>
       {!loading ? (
         <>
           <FileBox>
-            파일이름:{fileProps.filename} / 크기:{fileProps.size} / 업로드된 날짜:{' '}
+            파일이름: {fileProps.filename} / 크기: {fileProps.size} / 업로드된 날짜:{' '}
             {fileProps.uploadDate.year}-{fileProps.uploadDate.month}-{fileProps.uploadDate.day}
           </FileBox>
           <S.PasswordInputSection>
             <S.CheckPasswordInput
+              type="password"
               value={password}
               onKeyPress={(e: any) => {
                 if (e.key === 'Enter') {
@@ -103,7 +101,7 @@ export const CheckPasswordPage: React.FC = () => {
               onChange={(text) => {
                 setPassword(text.target.value.replace(/(\s*)/g, ''));
               }}
-              placeholder="비밀번호2를 입력해주세요."
+              placeholder="비밀번호를 입력해주세요."
             />
 
             <Button
