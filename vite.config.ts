ba6cd44546530afeef2e4dd43b0c1b87141373fb
minpeter <kail2005611@gmail.com>
2023-10-16 12:48:33 +0900

    Fix warning chunks are larger than 500 kB

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,11 @@ import viteTsconfigPaths from 'vite-tsconfig-paths'
 import svgr from 'vite-plugin-svgr'
 
 export default defineConfig({
-    // depending on your application, base can also be "/"
     base: '',
+    server: {
+      open: true,
+      port: 3000, 
+    },
     plugins: [
       svgr(),
       react({
@@ -14,12 +17,16 @@ export default defineConfig({
           plugins: ['@emotion/babel-plugin'],
         },
       }),
-      viteTsconfigPaths(),
+      viteTsconfigPaths()
     ],
-    server: {
-        // this ensures that the browser opens upon server start
-        open: true,
-        // this sets a default port to 3000  
-        port: 3000, 
-    },
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            react: ['react', 'react-dom'],
+            vendor: ['lodash', 'moment'],
+          },
+        },
+      }
+    }
 })
